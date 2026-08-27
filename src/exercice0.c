2cd94e00537c09354beada44dda0686571e6582a
Vianpyro <vianney+github@veremme.org>
2026-08-26 20:06:45 -0400

    No copy/paste or template -- oopsie

--- src/exercice0.c
+++ src/exercice0.c
@@ -9,19 +9,6 @@
 #include <stdlib.h> // pour EXIT_SUCCESS
 
 int main(void) {
-    int annee_actuelle;     // Année de la date d'aujourd'hui
-    int annee_naissance;    // Année de naissance de l'usager
-    int age = 0;
-
-    printf("Veuillez saisir l'annee actuelle : ");
-    scanf_s("%d", &annee_actuelle);
-
-    printf("Veuillez saisir votre annee de naissance : ");
-    scanf_s("%d", &annee_naissance);
-
-    // Calcul de l'age
-    age = annee_actuelle - annee_naissance;
-
-    printf("Au cas ou vous l'auriez oublie, vous avez : %d ans.\n", age);
+    printf("Hello TCH099!\n");
     return EXIT_SUCCESS;
 }
